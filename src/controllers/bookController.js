64fc0d12c5a1c62dd3f1726964343007a1f47da3
marcoshenriquefc <marcosh2267@gmail.com>
2023-01-09 13:27:42 -0300

    create Post with conection in DB

--- src/controllers/bookController.js
+++ src/controllers/bookController.js
@@ -6,6 +6,20 @@ class BookController {
             res.status(200).json(booksFind)
         } )
     }
+
+    static registerNewBook = (req, res) => {
+        let book = new books(req.body);
+
+        book.save((err) => {
+
+            if(err){
+                res.send({message: `${err.message} - Fala ao cadastrar um livro`})
+            }
+            else{
+                res.status(201).send(book.toJSON());
+            }
+        })
+    }
 }
 
 export default BookController
--- src/routes/booksRoutes.js
+++ src/routes/booksRoutes.js
@@ -5,5 +5,6 @@ const router = express.Router();
 
 router
     .get("/livros", BookController.listBooks)
+    .post("/livros", BookController.registerNewBook)
 
 export default router
